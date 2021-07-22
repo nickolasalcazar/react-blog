f8602dcc5c6efbbf64b3ade9047f48a84256d0ba
Nickolas Alcazar <nickolasalcazar@gmail.com>
2021-07-21 19:17:14 -0700

    Added components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
+  const title = 'Welcome';
+  const num_likes = 22;
+
   return (
     <div className="App">
-      App
+      <Navbar />
+      <div className='content'>
+        <Home />
+      </div>
     </div>
   );
 }
