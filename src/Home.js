7a852a78d7db717dcb558abbb938aa0f664b34b0
Nickolas Alcazar <nickolasalcazar@gmail.com>
2021-07-25 12:01:07 -0700

    Added useEffect

--- src/Home.js
+++ src/Home.js
@@ -1,15 +1,17 @@
-import React from "react";
+import { useState, useEffect } from 'react';
 import BlogList from './BlogList'
 
 /*
  * The Home component contains all of the content associated with the homepage of the app.
  */
 const Home = () => {
-    const [blogs, setBlogs] = React.useState([
+    const [blogs, setBlogs] = useState([
         {title: 'Blog 1', body: 'red', author: 'Mario', id: 1},
         {title: 'Blog 2', body: 'yellow', author: 'Yoshi', id: 2},
         {title: 'Blog 3', body: 'green', author: 'Luigi', id: 3}
     ]);
+
+    const [name, setName] = useState('Mario');
 
     const handleDelete = (id) => {
         // Filter for items that do not match the id
@@ -17,9 +19,15 @@ const Home = () => {
         setBlogs(newBlogs);
     }
 
+    useEffect(() => {
+        console.log('useEffect called');
+    }, [name]);
+
     return (
         <div className='Home'>
             <BlogList blogs={blogs} title='All Blogs' handleDelete={handleDelete}/>
+            <button onClick={() => setName('Luigi')}>Change name</button>
+            <p>{name}</p>
         </div>
     )
 }
