f0dfb2cf61d0378ab2910af9809d7bca6e0410ba
Nickolas Alcazar <nickolasalcazar@gmail.com>
2021-07-27 17:31:20 -0700

    Added POST request to db for creating new blog posts.

--- src/Create.js
+++ src/Create.js
@@ -1,10 +1,53 @@
+import { useState } from "react";
+
 const Create = () => {
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [author, setAuthor] = useState("Mario");
+    const [isPending, setIsPending] = useState(false);
+
+    const handleSubmit = (e) => {
+        e.preventDefault(); // Prevents page from automatically reloading upon form submission
+        const blog = { title, body, author };
+        setIsPending(true);
+        fetch('http://localhost:8000/blogs', {
+            method: 'POST',
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(blog)
+        }).then(() => setIsPending(false));
+    }
     return (
         <div className="create">
-            <h2>Create New Blog</h2>
-            <p>Create a new blog here!</p>
+            <h2>Make a Blog Post</h2>
+            <form onSubmit={handleSubmit}>
+                <label>Title:</label>
+                <input
+                    type="text"
+                    required
+                    value={title}
+                    onChange={(e)=>setTitle(e.target.value)}
+                />
+                <label>Body:</label>
+                <textarea
+                    required
+                    value={body}
+                    onChange={(e)=>setBody(e.target.value)}
+                ></textarea>
+                <label>Author:</label>
+                <select
+                    value={author}
+                    onChange={(e)=>setAuthor(e.target.value)}
+                >
+                    <option value="Yoshi">Yoshi</option>
+                    <option value="Mario">Mario</option>
+                </select>
+                { !isPending && <button>Post</button> }
+                { isPending && <button disabled>Submitting</button> }
+                <p>{ title }</p>
+                <p>{ body }</p>
+                <p>{ author }</p>
+            </form>
         </div>
     );
 }
-
 export default Create;
